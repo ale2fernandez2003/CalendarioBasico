0b0485fa7cb9c237aa668b5a12ce25f6208d2eac
Alejandro <ale2fernandez2003@gmail.com>
2022-12-19 10:01:33 +0100

    cambios

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -1,14 +1,10 @@
 
-/**
- * Write a description of class CalendarioBasico here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class CalendarioBasico
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int primero;
+    private int segundo;
+    private int tercero;
 
     /**
      * Constructor for objects of class CalendarioBasico
@@ -16,18 +12,9 @@ public class CalendarioBasico
     public CalendarioBasico()
     {
         // initialise instance variables
-        x = 0;
+        primero = 1;
+        segundo = 1;
+        tercero = 1;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
